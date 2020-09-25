33e25a40ac38457609c338d30c9318c398360cc9
Will Hagan <william_hagan@hotmail.com>
2020-09-24 23:05:24 -0500

    deleted sidebar.js

--- custom_reports/static/src/js/custom_reports_view.js
+++ custom_reports/static/src/js/custom_reports_view.js
@@ -6,7 +6,6 @@ var AbstractController = require('web.AbstractController');
 var AbstractModel = require('web.AbstractModel');
 var AbstractRenderer = require('web.AbstractRenderer');
 var AbstractView = require('web.AbstractView');
-//var ListRenderer = require('web.ListRenderer');
 var viewRegistry = require('web.view_registry');
 
 
@@ -14,12 +13,7 @@ var CustomReportsController = AbstractController.extend({});
 var CustomReportsRenderer = AbstractRenderer.extend({});
 var CustomReportsModel = AbstractModel.extend({});
 
-/*AbstractView.include({
-    init: function (parent, state, params)  {
-        this._super(parent, state, params);
-        this.withSearchBar = false;
-    },
-});*/
+
 var CustomReportsView = AbstractView.extend({
     withControlPanel: false,
     config: {
