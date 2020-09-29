ba298a5e594d307216edda69e77b142728e86b35
Daniel Chang <E1515551@regencyhealthcare.com>
2020-09-28 20:26:52 -0500

    models update

--- custom_reports/models/models.py
+++ custom_reports/models/models.py
@@ -2,11 +2,33 @@
 
 from odoo import models, fields, api
 
-
 class CustomReport(models.Model):
     _name = 'custom_reports.custom_report'
-    _description = "Custom Reports"
+    _description = 'Custom Reports'
 
-    name = fields.Char(string="Title", required=True)
+    # basic fields
+    name = fields.Char(string="Report Title", required=True)
+    url = fields.Char(required=True)
+    category = fields.Char()
     description = fields.Text()
+    delete_date = fields.Datetime()
 
+    # relational fields
+    delete_uid = fields.Many2one(comodel_name="res.users")
+
+    # auto
+    @api.multi
+    def action_url(self):
+        return {
+            'type' : 'ir.actions.act_url',
+            'url' : '/custom_reports/go?model=custom_reports.custom_report&field=url&id=%s'%(self.id),
+            'target' : 'new',
+        }
+
+# class CustomReport_History(models.Model):
+#     _name = 'custom_reports.custom_report_history'
+#     _description = 'Custom Reports History'
+
+#     report_id = fields.Many2one(related='custom_reports.custom_report.id', required=True)
+#     column_name = fields.Char(required=True)
+#     old_value = fields.Text()
