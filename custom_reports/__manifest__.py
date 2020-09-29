2798d1a380da15f1f67f1a2801a49016aeb2cc34
Will Hagan <william_hagan@hotmail.com>
2020-09-29 01:53:45 -0500

    models

--- custom_reports/__manifest__.py
+++ custom_reports/__manifest__.py
@@ -13,8 +13,8 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/assets.xml',
-        'views/custom_reports_views.xml',
         'views/employee_performance_views.xml',
+        'views/custom_reports_views.xml',
     ],
     'demo': [
         'demo/demo.xml',
--- custom_reports/models/models.py
+++ custom_reports/models/models.py
@@ -14,11 +14,23 @@ class CustomReport(models.Model):
 class EmployeePerformanceReport(models.Model):
     _name = 'custom_reports.employee_performance_report'
     _description = "Employee Performance Reports"
-
-    work_period = fields.Char(string="Work Period")
-    employee = fields.Char(string="Employee")
-    sales = fields.Float(string="Sales")
+    
+    employee_performance_id = fields.One2many('custom_reports.employee_performance', 'employee_performance_report_id', string="Employee Performances")
 
 
+class EmployeePerformance(models.Model):
+    _name = 'custom_reports.employee_performance'
+    _description = 'Employee Performance'
 
+    worked_hours_sum = fields.Float(string="Worked Hours", compute='worked_hours')
+    employee_performance_report_id = fields.Many2one('custom_report.employee_performance_report', string="Employee Performance Report")
+
+    def worked_hours(self):
+        total = 0
+        for attendance in self:
+            recordset = self.env['hr.attendance'].search_read([('employee_id', '=', attendance.employee_id.id)])
+            for record in recordset:
+                total = total + record.worked_hours
+        worked_hours_sum = total
+        
                 
