021d54bb75bc8e2416989a9dc350adc20b67afe4
Will Hagan <william_hagan@hotmail.com>
2020-10-08 15:42:35 -0500

    email market models

--- custom_reports/models/email_marketing.py
+++ custom_reports/models/email_marketing.py
@@ -10,3 +10,14 @@ class EmailMarketingReport(models.Model):
     # basic properties
     start_date = fields.Datetime(string='Start Date')
     end_date = fields.Datetime(string='End Date')
+    mass_mailing_ids = fields.Many2many('mailing.mailing', relation='custom_reports_email_marketing_report_mail_rel', column1='email_marketing_id', column2='mass_mailing_id', string="Mass Mailings")
+    
+
+
+class MassMailing(models.Model):
+    _inherit = 'mailing.mailing'
+
+    product_ids = fields.Many2many('product.product', relation='custom_reports_email_marketing_report_product_rel', column1='mass_mailing_id', column2='product_id', string="Products")
+
+    #email_marketing_report_ids = fields.Many2one('custom_reports.email_marketing_report', string="Email Marketing Report", ondelete='cascade', store=True)
+    
