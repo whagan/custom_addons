abc7d9877b1cd9c5cf77d6796d389ed3b586629f
Will Hagan <william_hagan@hotmail.com>
2020-10-11 15:09:31 -0500

    email market models

--- custom_reports/models/email_marketing.py
+++ custom_reports/models/email_marketing.py
@@ -19,14 +19,14 @@ class MassMailing(models.Model):
     product_ids = fields.Many2many('product.product', relation='custom_reports_email_marketing_report_product_rel', column1='mass_mailing_id', column2='product_id', string="Products")
 
     # sales with preset date ranges
-    sales_prev_avg = fields.Float(string="Avg Weekly Sales Prev Yr", store=True, readonly=True, compute="_compute_sales")
-    #sales_since_avg = fields.Float(string="Weekly Average Sales Since Mailing", store=True, readonly=True, compute="_compute_sales")
+    sales_prev_avg = fields.Float(string="Avg Weekly Sales Prev 6 mos", store=True, readonly=True, compute="_compute_prev_sales")
+    sales_since_avg = fields.Float(string="Avg Weekly Sales Since Mailing", store=True, readonly=True, compute="_compute_since_sales")
     
     # percent change
     #sales_avg_delta = fields.Float(string="Percent Change Average Sales", store=True, readonly=True, compute="_compute_avg_change")
 
     @api.depends('product_ids')
-    def _compute_sales(self):
+    def _compute_prev_sales(self):
         for record in self:
             total_sales = 0.0
             total_qty = 0
@@ -35,10 +35,29 @@ class MassMailing(models.Model):
                     product_sales = record.env['sale.order.line'].search([
                         ('product_id', '=', product_id.id),
                         ('state', 'in', ['sale', 'done']),
+                        ('order_id.date_order', '<', record.sent_date)
                     ])
                     for product_sale in product_sales:
                         total_sales += product_sale.price_subtotal
-                record.sales_prev_avg = total_sales
+                record.sales_prev_avg = total_sales / 26
             else:
-                record.sales_prev_avg = total_sales
+                record.sales_prev_avg = total_sales / 26
+    
+    @api.depends('product_ids')
+    def _compute_since_sales(self):
+        for record in self:
+            total_sales = 0.0
+            total_qty = 0
+            if record.product_ids:
+                for product_id in record.product_ids:
+                    product_sales = record.env['sale.order.line'].search([
+                        ('product_id', '=', product_id.id),
+                        ('state', 'in', ['sale', 'done']),
+                        ('order_id.date_order', '>=', record.sent_date)
+                    ])
+                    for product_sale in product_sales:
+                        total_sales += product_sale.price_subtotal
+                record.sales_since_avg = total_sales / 26
+            else:
+                record.sales_since_avg = total_sales / 26
      
