c0d477380e62d03edfe005746fa847bfaf3e2080
Will Hagan <william_hagan@hotmail.com>
2020-10-17 18:02:29 -0500

    sales location fix

--- custom_reports/models/sales_by_location.py
+++ custom_reports/models/sales_by_location.py
@@ -6,7 +6,7 @@ import datetime
 import logging
 _logger = logging.getLogger(__name__)
 
-
+ 
 #Sales By Location Report DataModel
 class SalesByLocationReport(models.Model):
     _name = 'custom_reports.sales_by_location_report'
@@ -15,7 +15,7 @@ class SalesByLocationReport(models.Model):
    # basic properties
     start_date = fields.Datetime(string='Start Date')
     end_date = fields.Datetime(string='End Date')
-    company_ids = fields.Many2many('sale.order', relation='sales_by_location_report_rel', column1='custom_report_id', column2='company_id', string="companies")
+    company_ids = fields.Many2many('res.company', relation='sales_by_location_report_rel', column1='custom_report_id', column2='company_id', string="companies")
     sales_by_location_ids = fields.One2many('custom_reports.sales_by_location', 'sales_by_location_report_id', string="Sales By Location")
 
     # methods
@@ -43,23 +43,24 @@ class SalesByLocation(models.Model):
     
     
     # properties
-    sale_id = fields.Many2one('sale.order', string="Company", ondelete='cascade', index=True, store=True)
-    company_id = fields.Many2one(related='sale_id.company_id', store=True, readonly=True)
+    company = fields.Many2one('res.company', string="Company", ondelete='cascade', index=True, store=True)
     sales_by_location_report_id = fields.Many2one('custom_reports.sales_by_location_report', string="Sales By location", ondelete='cascade', store=True)
+    
     start_date = fields.Datetime(related='sales_by_location_report_id.start_date', required=True)
     end_date = fields.Datetime(related='sales_by_location_report_id.end_date', required=True)
     
+    total_sales = fields.Float(string="Total Sales", compute="_calculate_total_sales", readonly=False, store=True)
     
     
     #computing totals of the sales by location in a given time period
-    @api.depends('sale_id','company_id','start_date','end_date')
+    @api.depends('company','start_date','end_date')
     def _calculate_total_sales(self):
         for record in self:
             total_sales = 00.0
-            if record.company_id and (record.start_date <= record.end_date):
+            if record.company and (record.start_date <= record.end_date):
                 orders = record.env['sale.order'].search([
+                    ('company_id', '=', record.company.id),
                     ('state', 'in', ['sale', 'done']),
-                    ('company_id', '=', record.sale_id.id),
                     ('date_order', '>=', record.start_date),
                     ('date_order', '<=', record.end_date)
                     ])
