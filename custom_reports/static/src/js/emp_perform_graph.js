735e844d8bb9d17ae62e296d13bf78e93046477c
whagan <william_hagan@hotmail.com>
2020-10-21 12:48:10 -0500

    emp perform graph

--- custom_reports/static/src/js/emp_perform_graph.js
+++ custom_reports/static/src/js/emp_perform_graph.js
@@ -6,6 +6,10 @@ odoo.define('custom_reports_view.EmpPerformGraph', function (require)   {
     var Registry = require('web.field_registry');
 
     // should var name below be same as name in line 1?
+    /**
+     * In Odoo, to create a custom widget, you typically create a view and then
+     * register that view with the field_registry.
+     */
     var EmpPerformGraph = BasicFields.FieldBinaryImage.extend({
-        
+
     })
