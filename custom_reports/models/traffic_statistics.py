3b5e451769e2bb96c4a18ebce6315fdd2b8509f3
Kevin Murphy <Kevincitorios@gmail.com>
2020-10-27 20:42:50 -0500

    fixed DB issues from previouse commit

--- custom_reports/models/traffic_statistics.py
+++ custom_reports/models/traffic_statistics.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 #Traffic Statistics Report DataModel
 class TrafficStatisticsReport(models.Model):
-    _name = 'custom_report.traffic_statistics_report'
+    _name = 'custom_reports.traffic_statistics_report'
     _description = 'Traffic Statistics Report'
 
    # Basic properties
@@ -17,20 +17,20 @@ class TrafficStatisticsReport(models.Model):
     traffic_statistics_ids = fields.One2many('custom_reports.traffic_statistics', 'traffic_statistics_report_id', string="Traffic Statistics")
 
 
-    # @api.model
-    #     def create(self, values):
-    #         record = super(TrafficStatisticsReport, self).create(values)
-    #         order_ids = values['order_ids'][0][2]
-    #         records = []
-    #         for company_id in company_ids:
-    #             records.append({
-    #                 'order': order_id,
-    #                 'traffic_statistics_report_id': record.id,
-    #                 'start_date': record.start_date,
-    #                 'end_date': record.end_date
-    #             })
-    #         self.env['custom_reports.traffic_statistics'].create(records)
-    #         return record
+    @api.model
+    def create(self, values):
+        record = super(TrafficStatisticsReport, self).create(values)
+        order_ids = values['order_ids'][0][2]
+        records = []
+        for company_id in company_ids:
+            records.append({
+                'order': order_id,
+                'traffic_statistics_report_id': record.id,
+                'start_date': record.start_date,
+                'end_date': record.end_date
+            })
+        self.env['custom_reports.traffic_statistics'].create(records)
+        return record
 
 
 
@@ -42,6 +42,7 @@ class TrafficStatistics(models.Model):
     # properties
     order_id = fields.Many2one('pos.order', string="order", ondelete='cascade', index=True, store=True)
     traffic_statistics_report_id = fields.Many2one('custom_reports.traffic_statistics_report', string="Traffic Statistics Report", ondelete='cascade', store=True)
+    
     start_date = fields.Datetime(related='traffic_statistics_report_id.start_date', required=True)
     end_date = fields.Datetime(related='traffic_statistics_report_id.end_date', required=True)
     
