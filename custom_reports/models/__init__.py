9a921d6d5738a622a8a736d7b86fd2991ef76df6
Kevin Murphy <Kevincitorios@gmail.com>
2020-11-08 13:28:28 -0600

    fixed models, views and security

--- custom_reports/models/__init__.py
+++ custom_reports/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # from . import filename_python_file_within_folder_or_subfolder
-from . import sales_by_location
+from . import sales_by_company
 from . import sales_statistics
 from . import custom_report
 from . import email_marketing
