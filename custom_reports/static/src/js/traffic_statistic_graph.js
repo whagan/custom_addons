8b4221d0eb8d3a7fccc5a07f8da7eca8c4af1490
Will Hagan <william_hagan@hotmail.com>
2020-11-11 14:35:40 -0600

    traffic statistics

--- custom_reports/static/src/js/traffic_statistic_graph.js
+++ custom_reports/static/src/js/traffic_statistic_graph.js
@@ -4,6 +4,8 @@ odoo.define('custom_reports.TrafficStatisticGraph', function(require)   {
     var BasicFields = require('web.basic_fields');
     var FieldRegistry = require('web.field_registry');
 
+    var COLORS = ['#875a7b', '#21b799', '#E4A900', '#D5653E', '#5B899E', '#E46F78', '#8F8F8F'];
+
     var TrafficStatisticGraph = BasicFields.FieldText.extend({
         jsLibs: [
             '/web/static/lib/Chart/Chart.js',
@@ -25,20 +27,22 @@ odoo.define('custom_reports.TrafficStatisticGraph', function(require)   {
          */
         start: function()   {
             var self = this;
-            // var colors = self._getColors(data.length);
-            // var datasets = []
+            
+            var datasets = [];
+            var colors = self._getColors(self.graph_data.length);
 
-            var labels = [];
-            // var data = [];
-
-            self.graph_data.forEach(graph_datum =>  {
-                labels.push(graph_datum.data.shop_id.data.display_name);
-            //     data.push(graph_datum.data.sales_hour.toFixed(2));
+            self.graph_data.forEach(function (graph_datum, index)   {
+                datasets.push({
+                    label: graph_datum.data.shop_id.data.display_name,
+                    data: JSON.parse(graph_datum.data.all_hour),
+                    borderColor: colors[index],
+                    fill: false
+                })
             });
 
-            console.log(labels);
+            console.log(datasets);
 
-            self.config = self._getLineGraph();
+            self.config = self._getLineGraph(datasets);
             self._loadChart();
             
         },
@@ -47,34 +51,36 @@ odoo.define('custom_reports.TrafficStatisticGraph', function(require)   {
         // Private
         // ----------------------------
 
-
          /**
          * Returns a line graph
          * @private
          */
-        _getLineGraph: function ()   {
+        _getLineGraph: function (datasets)   {
             return {
                 type: 'line',
                 data: {
                    labels: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM',
                             '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'],
-                   datasets: [{
-                       label: 'ClickIt July Sale Ad',
-                       data: [7, 5, 8, 5, 7, 10, 8, 7, 2, 4, 3, 1, 2, 3, 4, 5, 6, 6, 7, 3, 4, 5, 5, 9],
-                       borderColor: "#8e5ea2",
-                       fill: false
-                    },  {
-                        label: 'ClickIt August Sale Ad',
-                        data: [7, 5, 8, 5, 9, 10, 6, 7, 5, 4, 3, 1, 2, 3, 4, 5, 4, 4, 7, 3, 4, 7, 10, 9],
-                        borderColor: "#3cba9f",
-                        fill: false
-
-                    },  {
-                        label: 'ClickIt September Sale Ad',
-                        data: [4, 5, 3, 5, 6, 1, 6, 9, 5, 4, 5, 2, 2, 3, 4, 5, 7, 3, 7, 3, 4, 7, 8, 7],
-                        borderColor: "#c45850",
-                        fill: false
-                    }]
+                   datasets: datasets,
+                },
+                options:    {
+                    title:  {
+                        display: true,
+                        text: 'Sales per Hour in Day'
+                    },
+                    scales: {
+                        yAxes:  [{
+                            ticks:  {
+                                beginAtZero: true,
+                                callback: function(value, index, values)    {
+                                    value = value.toString();
+                                    value = value.split(/(?=(?:...)*$)/);
+                                    value = value.join('.');
+                                    return '$' + value;
+                                }
+                            }
+                        }]
+                    }
                 },
             };
         },
@@ -87,7 +93,19 @@ odoo.define('custom_reports.TrafficStatisticGraph', function(require)   {
             var canvas = this.$('canvas')[0];
             var context = canvas.getContext('2d');
             return new Chart(context, this.config);
-        }
+        },
+
+        /**
+         * Returns an array of colors
+         * @private
+         */
+        _getColors: function(length) {
+            var bgColors = [];
+            for (var i = 0; i < length; i++) {
+                bgColors.push(COLORS[i % COLORS.length]);  
+            }
+            return bgColors;
+        },
 
     });
 
