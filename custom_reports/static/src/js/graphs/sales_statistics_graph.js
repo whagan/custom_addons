234dec8377934d994802e15a8a1981c291639f71
Will Hagan <william_hagan@hotmail.com>
2020-11-22 16:27:56 -0600

    sales statistics graph fix

--- custom_reports/static/src/js/graphs/sales_statistics_graph.js
+++ custom_reports/static/src/js/graphs/sales_statistics_graph.js
@@ -14,7 +14,7 @@ odoo.define('custom_reports.SalesStatisticGraph', function(require)   {
             '/web/static/lib/Chart/Chart.js',
         ],
         template: 'sales_statistic_graph_template',
-        xmlDependencies: ['custom_reports/static/src/xml/sale_statistic_graph.xml'],
+        xmlDependencies: ['custom_reports/static/src/xml/sales_statistic_graph.xml'],
         
         /**
          * @override
@@ -23,6 +23,8 @@ odoo.define('custom_reports.SalesStatisticGraph', function(require)   {
             this._super.apply(this, arguments);
             this.graph_data = arguments[2].data.sales_statistic_ids.data;
             this.graph_type = this.attrs.options.graph_type;
+            console.log("HEY");
+            console.log(this);
         },
 
         /**
@@ -84,41 +86,6 @@ odoo.define('custom_reports.SalesStatisticGraph', function(require)   {
                 },
             };
         },
-
-        //  /**
-        //  * Returns a line graph
-        //  * @private
-        //  */
-        // _getLineGraph: function (labels, data, colors)   {
-        //     return {
-        //         type: 'line',
-        //         data: {
-        //            labels: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-        //            datasets: [{
-        //                label: 'ClickIt July Sale Ad',
-        //                data: [20, 22, 24, 27, 25, 44, 35, 27, 12, 8, 7],
-        //                borderColor: "#8e5ea2",
-        //                fill: false
-        //             },  {
-        //                 label: 'ClickIt August Sale Ad',
-        //                 data: [18, 20, 24, 29, 35, 37, 45, 50, 45, 40, 35],
-        //                 borderColor: "#3cba9f",
-        //                 fill: false
-
-        //             },  {
-        //                 label: 'ClickIt September Sale Ad',
-        //                 data: [27, 25, 29, 32, 30, 37, 55, 60, 65, 50, 30],
-        //                 borderColor: "#c45850",
-        //                 fill: false
-        //             },  {
-        //                 label: 'ClickIt October Sale Ad',
-        //                 data: [35, 29, 29, 32, 48, 50, 55, 60, 67, 70, 65],
-        //                 borderColor: "#3e95cd",
-        //                 fill: false
-        //             }]
-        //         },
-        //     };
-        // },
 
         /**
          * Returns a pie graph
@@ -184,7 +151,7 @@ odoo.define('custom_reports.SalesStatisticGraph', function(require)   {
                 data: {
                    labels: labels,
                    datasets: [{
-                       label: 'Sales by Company',
+                       label: 'Sales Statistics',
                        data: data,
                        backgroundColor: colors,
                     }]
